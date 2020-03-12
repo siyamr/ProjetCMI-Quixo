1f99cc8c512d9cc2cc241f4335661882b9bb8a79
Noonp <61587026+Noonp@users.noreply.github.com>
2020-03-12 13:53:32 +0100

    Add files via upload

---
+++ Matrices.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from random import randrange
+
+
+# Données initiales (pas de cellules vivante + création du damier avec matrice cell)
+def init():
+    for y in range(taille):
+        for x in range(taille):
+            etat[x][y] = neutre
+            cell[x][y] = canvas.create_rectangle((x*cote, y*cote, (x+1)*cote, (y+1)*cote), outline="black", fill="white")
+
+            
+taille = 5  #tailleeur du tableau
+cote = 75  #côté d'une cellule
+neutre = 0   #case blanche
+
+
+# Matrices
+cell = [[0 for i in range(taille)] for j in range(taille)]    #mémorise les carrés qui représentent les cellules
+etat = [[neutre for i in range(taille)] for j in range(taille)] #mémorise les statuts des cellules (neutre ou vivant)
+
+# Lancement du programme
+fenetre = Tk()
+fenetre.title("Quixo")
+
+#Titre dans la fenêtre
+canvas = Canvas(fenetre, width=900, height=600)
+canvas.pack()
+
+#fonctions pour lancer le Quixo
+init()
+
+fenetre.mainloop()
