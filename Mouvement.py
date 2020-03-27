a59c40423b32b1b735b5acc6b0df16b52ab91479
Gaiko19 <47215835+Gaiko19@users.noreply.github.com>
2020-03-27 13:12:40 +0100

    Update Mouvement.py

--- Mouvement.py
+++ Mouvement.py
@@ -1,47 +1,102 @@
+
 import Quixo.py
-global case[x] = 1 #case jouée [0-15]
-global mvt = "" #Endroit ou la case est déplacée G,D,B,H
+case[X] = ?         #case jouée avec des coordonnées en x et y
+global mvt = ""     #Endroit ou la case est déplacée G,D,B,H
+# petit "x"  = coordonné en abscisse  petit "y" = coordonné en ordonné   grand "X" = case jouée (combinaison de "x" et "y" à déterminer)
+
+#--------Définitions fonctions----------#
 
 
-def depgauche():
+def verifInput():
+    while (mvt != 'G' or mvt != 'D' or mvt != 'B' or mvt != 'H'):
+        print("Entré invalide, veuillez choisir l'une des propositions ci-contre [G]auche, [D]roite, [B]as, [H]aut")
+        mvt = input()
+        mvt = mvt.upper()
 
-def depdroite():
-    global tmp = case[x]
-    case[x] = 3
-    for i in range(1, 5):
-        if case[i] == 3:
+
+def depgauche():    # Déplacement case jouée à gauche
+    tmp = case[X]
+    for i in range(x,0):
+        if i-1 < 0:
+            case[0] = tmp
             break
-        if x + i > 4:
-            case[0]= case[x]
-        else :
-            case[x + i] = tmp
+        case[i] = case[i-1]
+
+def depdroite():    # Déplacement case jouée à droite
+    tmp = case[X]
+    for i in range(x,4):
+        if i+1 > 4:
+            case[4] = tmp
+            break
+        case[i] = case[i+1]
+
+def depbas():    # Déplacement case jouée en bas
+    tmp = case[X]
+    for i in range(y,4):    # y = la coordonnée en ordonné
+        if i+1 > 4:
+            case[4] = tmp
+            break
+        case[i] = case[i+1]
 
 
-def depbas():
-
-def dephaut():
-
+def dephaut():    # Déplacement case jouée en haut
+    tmp = case[X]
+    for i in range(y,0):
+        if i-1 < 0:
+            case[0] = tmp
+            break
+        case[i] = case[i-1]
 
 
 def direction():
     if mvt == "G":
-        depgauche
+        if x!=0:    # x = coordonné en x (abscisse)
+            depgauche()
+        else:
+            while mvt == "G":
+                print("Vous êtes déja de ce côté du plateau.\n Choissisez une autre direction. D ; B ; H")
+                mvt = input()
+                verifInput()
+                direction()
     elif mvt == "D":
-        depdroite
-    elif mvt == "B":
-        depbas
-    else:
-        dephaut
+        if x!=4:    # x = coordonné en x (abscisse)
+            depdroite()
+        else:
+            while mvt == "D":
+                print("Vous êtes déja de ce côté du plateau.\n Choissisez une autre direction. G ; B ; H")
+                mvt = input()
+                verifInput()
+                direction()
 
+    elif mvt == "B":
+        if y!=4:    # x = coordonné en x (abscisse)
+            depbas()
+        else:
+            while mvt == "B":
+                print("Vous êtes déja de ce côté du plateau.\n Choissisez une autre direction. G ; D ; H")
+                mvt = input()
+                verifInput()
+                direction()
+
+    else:
+        if y!=0:    # x = coordonné en x (abscisse)
+            dephaut()
+        else:
+            while mvt == "H":
+                print("Vous êtes déja de ce côté du plateau.\n Choissisez une autre direction. D ; B ; H")
+                mvt = input()
+                verifInput()
+                direction()
+
+
+
+#-----------Programme-----------#
 
 print("Ou voullez-vous déplacer la case ? [G,D,B,H]")
 str mvt = input()
 mvt = mvt.upper()
 
+verifInput()
 
-while (mvt != 'G' or mvt != 'D' or mvt != 'B' or mvt != 'H'):
-    print("Entré invalide, veuillez choisir l'une des propositions ci-contre [G]auche, [D]roite, [B]as, [H]aut")
-    mvt = input()
-    mvt = mvt.upper()
 
-direction
+direction()
